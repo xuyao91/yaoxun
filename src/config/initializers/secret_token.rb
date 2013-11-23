7f9368867944f26975418f745ceea3dd6aa9cf44
xuyao <xu.yao@vcooline.com>
2013-11-23 21:15:22 +0800

    项目初始化

---
+++ src/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yaoxun::Application.config.secret_token = '12d736e99af644451e73bf8b90a512ed197922730ad5a6de13c33f3347cba83f6f2a74cc685fe42fc25b1b3f40489ca0e28f933d85a476446c41fd8e0125904b'
